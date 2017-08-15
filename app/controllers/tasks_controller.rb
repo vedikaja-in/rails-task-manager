c2d12e97ab856bf1d80eb309fda967b0501a61c7
Vedika Jain <vedikajain.350@gmail.com>
2017-08-15 14:40:11 +0200

    Completed first exercise

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :done)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show', as: "task"
+  post '/tasks/', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# GET '/tasks': get all your tasks.
+# GET '/tasks/:id': get a precise task, e.g GET '/tasks/3' fetches the task with id=3
+# GET '/tasks/new': get the form to create a new task
+# POST '/tasks': post a new task
+# GET '/tasks/:id/edit': get the form to edit an existing task
+# PATCH '/tasks/:id': update an existing task
+# DELETE '/tasks/:id': delete an existing task
